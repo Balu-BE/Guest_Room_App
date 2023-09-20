6388dccf69fe051ee53c3374300e754c05ca8eac
BALU R <143741920+Balu-BE@users.noreply.github.com>
2023-09-20 17:22:20 +0530

    Add files via upload

---
+++ Guest_Room_Booking_Application/largeimg.php
@@ -0,0 +1,21 @@
+  
+            <!-- Contact Details Column -->
+            <div class="col-lg-8">
+                <h3>Contact Details</h3>
+                <p>
+                    34th Nehru Street<br>Sathyamangalam, Erode<br>
+                </p>
+                <p><i class="fa fa-phone"></i> 
+                    <abbr title="Phone">P</abbr>: +91 45645-78920</p>
+                <p><i class="fa fa-envelope-o"></i> 
+                    <abbr title="Email">E</abbr>: <a href="mailto:balurcse20@jkkmct.edu.in">Housematebooking@gmail.com</a>
+                </p>
+                <p><i class="fa fa-clock-o"></i> 
+                    <abbr title="Hours">H</abbr>: Monday - Friday: 9:00 AM to 5:00 PM</p>
+                
+            </div>
+              <div class="col-lg-4"> 
+        <div class="row">
+          <?php  require_once('sidebar.php') ; ?>
+        </div>
+      </div>
