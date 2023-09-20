0140cfd3f2efa1c7e205f40349dd3f71d32e97b5
BALU R <143741920+Balu-BE@users.noreply.github.com>
2023-09-20 16:24:06 +0530

    Add files via upload

---
+++ booking/mpesa.php
@@ -0,0 +1,7 @@
+<td valign="top" class="body" style="padding-bottom:10px;">
+  <div id="mpesa">THANK YOU FOR MAKING YOUR RESRVATION .
+    TO PAY YOUR BILL VIA ANY UPI APPS USE THIS NUMBER 
+    1234567890. YOU ARE EXPECTED TO PAY <?PHP echo $_SESSION['pay']; ?>
+	FOR THE MOST RECENT TRANSCTION YOU MADE.
+  </div>
+<td>
